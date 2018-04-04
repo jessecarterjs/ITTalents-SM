daa46b1c934c6004a588d5de9d185a9e452c8e96
Ana Todorova <todorova@outlook.com>
2018-04-04 08:42:47 +0300

    add reply

--- controllers/timelineController.js
+++ controllers/timelineController.js
@@ -11,7 +11,16 @@ function timelineController() {
         var timeline = Handlebars.compile(source);
         var html = timeline({posts: posts});
         $('#allposts').html(html);
-   }
+
+        $('#reply-btn').on('click', function(){
+            var reply = $('.input-reply').val();
+            var id = $('.input-reply').parent().parent().attr('id')
+            var username = JSON.parse(sessionStorage.getItem('loggedUser'));
+            usersTimeline.addReply(reply, username, id)
+            showTimeline();
+        })
+   };
+   
    $('.add-img').on('click', function (event) {
       if ($('#input-add-img').css('display') != 'inline-block') {
       	$('#input-add-img').show()
@@ -24,6 +33,7 @@ function timelineController() {
         var post = $('#new-post-input').val();
         var img = $('#input-add-img').val();
         var username = JSON.parse(sessionStorage.getItem('loggedUser'));
+        console.log(username)
         usersTimeline.addPost(post, img, username);
         
         $('#new-post-input').val(''); //clear input field after post
@@ -31,5 +41,7 @@ function timelineController() {
         $('#input-add-img').hide(); //hide img input after post
         showTimeline();
    });
+
+   
    showTimeline();
 };
--- logRegServices/postsServices.js
+++ logRegServices/postsServices.js
@@ -1,28 +1,49 @@
 var usersTimeline = (function () {
 
+    var nextId = 1;
+
     function Post(text, img, username) {
-       this.text = text;
-       this.img = img;
-       this.username = username;
+        this.id = nextId++;
+        this.text = text;
+        this.img = img;
+        this.username = username;
+        this.replies = [];
+        
     }
 
-    function Timeline (){
-        if(localStorage.getItem("timelines") != null){
-            this.timelines = JSON.parse(localStorage.getItem("timelines"));   
+    function Reply(text, username) {
+        this.text = text;
+        this.username = username;
+    }
+
+    function Timeline() {
+        if (localStorage.getItem("timelines") != null) {
+            this.timelines = JSON.parse(localStorage.getItem("timelines"));
         } else {
-           this.timelines = []; 
-       }
+            this.timelines = [];
+        }
     }
 
     Timeline.prototype.addPost = function(text, img, username){
         var post = new Post (text, img, username);
         this.timelines.unshift(post); // тук трябва да е unshift вместо push за да може най-новия пост да излиза най-отгоре 
         localStorage.setItem("timelines", JSON.stringify(this.timelines));
-    }
-    Timeline.prototype.getPosts = function(username) {
-        return this.timelines.filter(function(post) {
+    };
+
+    Timeline.prototype.getPosts = function (username) {
+        return this.timelines.filter(function (post) {
             return post.username == username;
         })
+    };
+
+    Timeline.prototype.addReply = function (text, username, id) {
+        var reply = new Reply(text, username)
+        var index = this.timelines.findIndex(function (post) {
+            return post.id == id;
+        })
+        this.timelines[index].replies.push(reply);
+        localStorage.setItem("timelines", JSON.stringify(this.timelines));
     }
+
     return new Timeline()
 })()
